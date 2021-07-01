ef6e82d26c074d02c8279a03fea3c40e5348a563
LilianMathu <lilianmathu2018@gmail.com>
2021-07-01 13:29:26 +0200

    add rootpath

--- Include/Config.php
+++ Include/Config.php
@@ -25,7 +25,7 @@ $sDATABASE = 'churchcrm';
 // - the path SHOULD Start with slash, if not ''.
 // - the path SHOULD NOT end with slash.
 // - the is case sensitive.
-$sRootPath = '';
+$sRootPath = '/';
 
 // Set $bLockURL=TRUE to enforce https access by specifying exactly
 // which URL's your users may use to log into ChurchCRM.
