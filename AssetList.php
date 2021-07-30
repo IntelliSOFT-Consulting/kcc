3a32984de4d77e49411c62609c435ff69e1e68f8
Lilian Mathu <lilianmathu2018@gmail.com>
2021-07-30 10:42:36 +0300

    Asset list

--- AssetList.php
+++ AssetList.php
@@ -1,26 +1,76 @@
 <?php
-
-//Include the function library
 require 'Include/Config.php';
 require 'Include/Functions.php';
-require 'Include/CanvassUtilities.php';
 
-//Add classes
-
-use ChurchCRM\dto\SystemConfig;
-use ChurchCRM\Note;
 use ChurchCRM\Utils\InputUtils;
-use ChurchCRM\dto\SystemURLs;
-use ChurchCRM\Emails\NewPersonOrFamilyEmail;
-use ChurchCRM\Utils\RedirectUtils;
-use ChurchCRM\Bootstrapper;
 use ChurchCRM\Authentication\AuthenticationManager;
 
-//Set page title
-$sPageTitle = gettext('All Assets');
-
-
+///////////////////////
 require 'Include/Header.php';
 
+// display all assets
+$sSQL = "SELECT * from assets WHERE assetDeleted='False'";
+$result = RunQuery($sSQL);
+$resultCheck = mysqli_num_rows($result);
+
+
+if (isset($_POST['Action']) && isset($_POST['assetID']) && AuthenticationManager::GetCurrentUser()->isAddRecordsEnabled()) {
+  $assetID = InputUtils::LegacyFilterInput($_POST['assetID'], 'int');
+  $action = InputUtils::LegacyFilterInput($_POST['Action']);
+
+  if ($action == 'Delete' && $assetID) {
+    $sSQL = "UPDATE assets SET assetDeleted = 'True' WHERE assetID='$assetID'  LIMIT 1";
+    RunQuery($sSQL);
+    
+  } 
+ 
+}
 ?>
 
+<!-- HTML TABLE -->
+<div class="box box-warning">
+  <div class="box-body">
+    <table id="assetlist" class='table data-table table-striped table-bordered table-responsive'>
+      <thead>
+        <tr>
+          <th><?= gettext('Name') ?></th>
+          <th><?= gettext('Make') ?></th>
+          <th><?= gettext('Quantity') ?></th>
+          <th><?= gettext('Category') ?></th>
+          <th><?= gettext('Action') ?></th>
+        </tr>
+      </thead>
+
+      <tbody>
+
+        <!--Populate the table with asset details -->
+        <?php
+        while ($row = mysqli_fetch_assoc($result)) {
+        ?>
+          <tr>
+            <td><?php echo $row['assetName'] ?></td>
+            <td><?php echo $row['make'] ?></td>
+            <td><?php echo $row['quantity'] ?></td>
+            <td><?php echo $row['assetCategory'] ?></td>
+            <td>
+            <a href="AssetView.php?view=<?php echo $row['assetID']; ?>" class="btn btn-info" name="view"><span class="fa fa-eye"></span></a>
+
+              <form style="display:inline-block" name="DeleteAsset" action="AssetList.php" method="POST">
+                <input type="hidden" name="assetID" value="<?= $row['assetID']; ?>">
+                <button type="submit" name="Action" title="<?= gettext('Delete') ?>" data-tooltip value="Delete" class="btn btn-danger" onClick="return confirm('Deleting an asset will also delete all assignments for that asset.  Are you sure you want to DELETE asset ID: <?= $row['assetID']; ?>')">
+                  <i class='fa fa-trash'></i>
+                </button>
+              </form>
+
+            </td>
+          </tr>
+        <?php } ?>
+      </tbody>
+    </table>
+  </div>
+</div>
+
+
+<?php
+require 'Include/Footer.php'
+?>
